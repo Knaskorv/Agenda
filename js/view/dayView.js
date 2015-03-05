9d516ed5cbf0bfd90592e650dfa10da6289a49c1
hansdo <hansdo@kth.se>
2015-03-05 17:53:34 +0100

    Added method to update time for day. +  Add a day at start

--- js/view/dayView.js
+++ js/view/dayView.js
@@ -8,15 +8,51 @@ var DayView = function (container, model) {
 	this.dayActivitesTable = container.find("#dayActivitesTable");
 	this.addDayButton = container.find("#addDayButton");
 
+	//To update the time set for that day
+	this.showTime = function(){
+		console.log('I  showTime');
+	this.startTimeDay= container.find('#startTimeDay');
+	this.endTimeDay= container.find('#endTimeDay');
+	this.totalTimeDay= container.find('#totalTimeDay');
+		for(var i=0; i<model.days.length; i++){
+			this.startTimeDay.attr("value",(model.days[i].getStart()));
+			this.endTimeDay.html(model.days[i].getEnd());
+			this.totalTimeDay.html(model.days[i].getTotalLength());
+		}
+	
+	}
+
+
+	 // skapa en dag som finns från start av program, nånstannsför anv attbörja
+	this.addANewDay = function (){
+		model.addDay(8,4);
+		var dayTable = this.dayActivitesTable.clone(); 
+		dayTable.on("dragover", {dndInfo:[model.days.length-1, 0]}, model.dragAndDrop);
+		dayTable.on("drop", {dndInfo:[model.days.length-1, 0]}, model.dragAndDrop);
+		this.showTime();
+	};
+	
+	this.addANewDay();
+
+	console.log('Langd på days lista: '+model.days.length);
+
+	
+	
+
 	var self = this; 
 
 	this.addDayButton.click(function(){
 		model.addDay(9, 10); 
+		console.log('I  addDayButton');
+		self.showTime();
 		var dayTable = self.dayActivitesTable.clone(); 
 		dayTable.on("dragover", {dndInfo:[model.days.length-1, 0]}, model.dragAndDrop);
 		dayTable.on("drop", {dndInfo:[model.days.length-1, 0]}, model.dragAndDrop);
 		container.append(dayTable);
+		console.log('Langd på days lista: '+model.days.length);
 	});
+
+console.log('Days: ' +model.days.length);
 
 	model.addObserver(this);
 
@@ -27,10 +63,11 @@ var DayView = function (container, model) {
 		$('tr:not(:first)', this.dayActivitesTable).remove();
 		
 		console.log('langd');
-		for(var i = 0; i < model.days[0]._activities.length; i++) {
+		for(var i = 0; i < model.days[0]._activities.length; i++) {//fastnade här för days var tom
 			console.log('HIT2');
 			var tr = $('tr.activity', this.dayActivitesTable).clone().removeClass('activity');
 			//var tr = document.getElementById("test"); 
+
 			var length = model.days[0]._activities[i].getLength();
 			var name = model.days[0]._activities[i].getName();
 			var color; 
