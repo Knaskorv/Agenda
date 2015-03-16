5932be4d0f7f6a8743a1f9ff45ecbccd45da467e
hansdo <hansdo@kth.se>
2015-03-16 10:59:29 +0100

    Cleaned up.

--- js/view/addActivityController.js
+++ js/view/addActivityController.js
@@ -4,13 +4,10 @@ var AddActivityController = function (view, model) {
 	// Get all the relevant elements of the view (ones that show data
   	// and/or ones that responed to interaction)
 
-
-	
-
 	view.addActivityDoneButton.click(function(){
 		document.getElementById("addActivityView").style.display = "none"; 
 		document.getElementById("blackout").style.display = "none"; 
-		//console.log(view.activityName.val()+' '+view.activityLength.val()+' '+view.activityType.val()+' '+view.activityDescription.val());
+		
 		var activity = new Activity(view.activityName.val(), view.activityLength.val(), view.activityType.val(), view.activityDescription.val());
 		model.addParkedActivity(activity, 0)
 
